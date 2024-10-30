cf42248b76ac71f8758a422b27800068a8bb8d8d
Ani Torosyan <ani.torosyan@volo.global>
2024-10-30 22:56:33 +0400

    Add UI test for forecast item

--- src/app/components/forecast-item.component.spec.ts
+++ src/app/components/forecast-item.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { signal } from '@angular/core';
 import { ForecastItemComponent } from './forecast-item.component';
 
 describe('ForecastItemComponent', () => {
@@ -13,4 +14,38 @@ describe('ForecastItemComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+  it('should check ui classes', () => {
+    const fixture = TestBed.createComponent(ForecastItemComponent);
+    const component = fixture.componentInstance;
+    let item = signal({
+      location: {
+        name: 'London',
+      },
+      current: {
+        condition: {
+          text: 'Sunny',
+        },
+        temp_c: 20,
+      },
+    });
+    component.item = item as any;
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('.sunny')).toBeTruthy();
+    item = signal({
+      location: {
+        name: 'London',
+      },
+      current: {
+        condition: {
+          text: 'Overcast',
+        },
+        temp_c: 20,
+      },
+    });
+    component.item = item as any;
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('.overcast')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('mat-card-title').textContent).toContain('London');
+  });
+
 });
